204240a8974ea18e7f3ba8b9c164d7773dbee8ca
wainepereira <wainepereira@gmail.com>
2025-01-20 18:11:54 -0300

    esta pronto o projeto

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Árvore em CSS</title>
+  <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+<div class="container">
+    <div class="base"></div>
+    <div class="cup">
+        <div class="top">
+            <div class="vapor">
+              <span class="--i:1;"></span>
+              <span class="--i:3;"></span>
+              <span class="--i:16;"></span>
+              <span class="--i:5;"></span>
+              <span class="--i:13;"></span>
+              <span class="--i:20;"></span>
+              <span class="--i:6;"></span>
+              <span class="--i:7;"></span>
+              <span class="--i:10;"></span>
+              <span class="--i:8;"></span>
+              <span class="--i:17;"></span>
+              <span class="--i:11;"></span>
+              <span class="--i:12;"></span>
+              <span class="--i:14;"></span>
+              <span class="--i:2;"></span>
+              <span class="--i:9;"></span>
+              <span class="--i:15;"></span>
+              <span class="--i:4;"></span>
+              <span class="--i:19;"></span>
+    
+            </div>
+            <div class="circle">
+                <div class="coffe">
+
+                </div>
+            </div>
+        </div>
+        <div class="alca"></div>
+    </div>
+</div>
+  </div>
+</body>
+</html>
